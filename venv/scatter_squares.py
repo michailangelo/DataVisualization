57519df7165254b0cd49a90e233969dc315f5d93
Michail Samartzis <michail.samartzis@eurodyn.com>
2019-08-22 14:21:46 +0300

    Edited plot graph.

--- venv/scatter_squares.py
+++ venv/scatter_squares.py
@@ -1,11 +1,17 @@
 import matplotlib.pyplot as plt
 
-plt.scatter(2,4, s = 2000)
+
+x_values = list(range(1, 1001))
+y_values = [x**2 for x in x_values]
+
+plt.scatter(x_values, y_values, edgecolor ='none', s = 40)
 
 # Set chart title and label axes
 plt.title("Square Numbers", fontsize=24)
 plt.xlabel("Value", fontsize=14)
 plt.ylabel("Square of Value", fontsize=14)
+
+plt.axis([0, 1100, 0, 1100000])
 
 # Set size of tick labels
 plt.tick_params(axis='both', which='major', labelsize = 14)

bfa6e8cc19da3fbbbfe96fdf72b421b86d48c5c1
Michail Samartzis <michail.samartzis@eurodyn.com>
2019-08-23 12:02:38 +0300

    Added colors and save funtion.

--- venv/scatter_squares.py
+++ venv/scatter_squares.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 x_values = list(range(1, 1001))
 y_values = [x**2 for x in x_values]
 
-plt.scatter(x_values, y_values, edgecolor ='none', s = 40)
+plt.scatter(x_values, y_values, c = y_values, cmap = plt.cm.Blues,  edgecolor ='none', s = 40)
 
 # Set chart title and label axes
 plt.title("Square Numbers", fontsize=24)
@@ -15,4 +15,4 @@ plt.axis([0, 1100, 0, 1100000])
 
 # Set size of tick labels
 plt.tick_params(axis='both', which='major', labelsize = 14)
-plt.show()
+plt.savefig('squares_plot.png', bbox_inches ='tight')
